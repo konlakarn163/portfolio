2d80caf3986b0429a1ea5444ef5e4c3a0beb9223
Konlakarn <buapian01@gmail.com>
2026-03-16 13:09:15 +0700

    Refactor Navbar for link redirect

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,9 +2,11 @@ import { useLayoutEffect, useRef } from "react";
 import gsap from "gsap";
 import LanguageSwitcher from "./LanguageSwitcher";
 import ThemeToggle from "./ThemeToggle";
+import { useNavigate } from "react-router-dom";
 
 export default function Navbar() {
   const navRef = useRef<HTMLElement>(null);
+  const navigate = useNavigate();
 
   useLayoutEffect(() => {
     const ctx = gsap.context(() => {
@@ -16,7 +18,7 @@ export default function Navbar() {
           opacity: 1,
           duration: 2,
           ease: "power3.out",
-        }
+        },
       );
     }, navRef);
 
@@ -26,7 +28,10 @@ export default function Navbar() {
   return (
     <header ref={navRef} className="fixed top-0 z-[999] w-full bg-base-dark">
       <div className="mx-auto container px-4 h-16 flex items-center justify-between">
-        <div className="font-extrabold text-lg space-x-1 font-header">
+        <div
+          className="font-extrabold text-lg space-x-1 font-header cursor-pointer hover-scale"
+          onClick={() => navigate("/")}
+        >
           <span className="tracking-widest text-cyan-500">Bas</span>
           <span className="text-base-white">Portfolio</span>
         </div>
